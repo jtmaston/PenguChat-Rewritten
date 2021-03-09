d0a5a93b0bbc06cdd19f7ebe4b807c22b28a6e80
jtmaston <jtmstn1@gmail.com>
2021-03-09 17:56:10 +0200

    Version 2.0 pre-alpha
    [ client.py ]
    Implemented consumer-producer file sending, removing the theoretical file size limit.
    Implemented FauxMessage, a helper class used for adding files to the chat box, much easier.
    Added debug prints, which probably won't be necessary anymore, but for now makes logging much easier
    Added terminator to all packets
    Fluidized how calls to transport are made, in order to make sure packets don't stack.
    Split the send and send_file methods, because they were incompatible
    Removed redundant "command" params to some methods
    Should have fixed the add_common_key bug
    Minor tweaks to the UI, more on that in the [ UIElements.py ] changelog
    Added a method to ingest files
    Fixed some typos in comments
    Minor updates have been done to the command processing part
    Fixed some bad logs
    
    [ chat.kv ]
    Added FileBubble class for the new UI
    Tweaked the request button, it will now display number of requests on it.
    Refactored sending dialogue
    
    [ Client/DBHandler.py ]
    Changed log level for now
    
    [ Server/DBHandler.py ]
    Updated content field to hold binary data, no longer text
    
    [ UIElements.py ]
    Removed LeftFile and RightFile, as they are now obsolete
    Added FileBubble, to house the files
    
    [ server.py ]
    Added proper file handling
    Moved command processing to its own method
    Server will now switch to file transfer mode, ignoring all commands on uplink with partner until FT is complete
    Added method to check if client is ready to receive file

--- Client/UIElements.py
+++ Client/UIElements.py
@@ -1,7 +1,11 @@
-from kivy import Logger
+from tkinter.filedialog import SaveAs, asksaveasfile
+
+from kivy import Logger, LOG_LEVELS
+
+Logger.setLevel(LOG_LEVELS["debug"])
 from kivy.base import ExceptionHandler, ExceptionManager
 from kivy.graphics.context_instructions import Color
-from kivy.graphics.vertex_instructions import Rectangle, RoundedRectangle
+from kivy.graphics.vertex_instructions import Rectangle, RoundedRectangle, Ellipse
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
@@ -17,15 +21,6 @@ colors = {
     'outgoing_message': (0.096, 0.535, 0.656),
     'incoming_message': (0, 0.213, 0.28)
 }
-class RightFile(Button):
-    def __init__(self, filedata=None, **kwargs):
-        super(RightFile, self).__init__(**kwargs)
-        self.text = filedata['filename']
-        self.file_data = filedata['file_blob']
-
-
-class LeftFile(Button):
-    pass
 
 
 class MenuButton(Button):
@@ -75,14 +70,48 @@ class MessageBubble(Label):
 
         self.bind(pos=self.update_rect)
 
-    def update_rect(self, value, new_position):
+    def update_rect(self, value=None, new_position=None, **kwargs):
         self.rect.pos = (self.parent.width - self.width, self.pos[1]) \
-                if self.side == 'r' \
-                else self.pos
+            if self.side == 'r' \
+            else self.pos
         self.rect.size = self.size
         if self.width > 0.75 * self.parent.width:
             self.text_size[0] = 0.75 * self.parent.width
         self.parent.height = self.height
+
+
+class FileBubble(Button):
+    def __init__(self, side, text, blob, **kwargs):
+        super(FileBubble, self).__init__(**kwargs)
+        self.background_color = (0, 0, 0, 0)
+        with self.canvas.before:
+            self.bc = Color()
+            self.bc.rgb = colors['incoming_message'] if side == 'l' else colors['outgoing_message']
+            self.rect = RoundedRectangle(pos=self.pos, size=(150, 150))
+            self.rect.radius = [(15, 15), (15, 15), (15, 15), (15, 15)]
+            self.side = side
+            self.data = blob
+            self.text =	f'\n\n\n{text}'
+
+        self.bind(pos=self.update_rect)
+        self.bind(on_press=self.callback)
+
+    def update_rect(self, *args, **kwargs):
+        self.rect.pos = (self.parent.width - self.width, self.pos[1]) \
+            if self.side == 'r' \
+            else self.pos
+        self.font_size = 0.15 * self.width
+        self.rect.size = self.size
+        self.parent.height = self.height
+
+    def callback(self, *args, **kwargs):
+        f = asksaveasfile(mode='wb+', initialfile=self.text.strip())
+        if f is None:
+            pass
+        f.write(self.data)
+        f.close()
+
+
 
 
 class SidebarElement:
@@ -121,15 +150,20 @@ class ConversationElement:
         self.left = None
         self.right = None
         self.line.size_hint_y = None
+        self.reload = None
 
         if side == 'l':
-            self.left = MessageBubble(text=text, side=side) if not isfile else LeftFile(filedata)
+            self.left = MessageBubble(text=text, side=side) if not isfile \
+                else FileBubble(side=side, text=filedata['filename'], blob=filedata['file_blob'])
             self.left.background_color.rgb = colors['incoming_message']
             self.right = EmptyWidget()
+            self.reload = self.left.update_rect
         else:
-            self.right = MessageBubble(text=text, side=side) if not isfile else MenuButton(text=filedata['filename'])
+            self.right = MessageBubble(text=text, side=side) if not isfile \
+                else FileBubble(side=side, text=filedata['filename'], blob=filedata['file_blob'])
             self.right.background_color.rgb = colors['outgoing_message']
             self.left = EmptyWidget()
+            self.reload = self.right.update_rect
 
         self.line.add_widget(self.left)
         self.line.add_widget(self.right)
